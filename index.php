07afc47e8d0d1b9e902a57dd8d8d0902b22c7f29
root <sites@hubmed.org>
2010-08-08 15:39:53 +0000

    using geoNear to get distances

--- index.php
+++ index.php
@@ -1,19 +1,20 @@
 <? require '/opt/libapi/main.php'; ?>
-<? 
+<? Config::set('DEBUG', 'FIRE'); ?>
+<?
 if (isset($_GET['latitude']) && isset($_GET['longitude'])) {
   require __DIR__ . '/update.php';
-  require __DIR__ . '/query.php'; 
+  require __DIR__ . '/query.php';
 }
 ?>
 <!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
-    <title>London Cycle Hire Stations</title> 
+    <title>London Cycle Hire Stations</title>
     <link id="shorturl" rev="canonical" type="text/html" href="http://bit.ly/bikesplz">
-    
-    <meta name="application-name" content="CycleWire"> 
-    <meta name="application-url" content="http://alf.hubmed.org/2010/07/london-cycle-hire/">  
+
+    <meta name="application-name" content="Cycle Hire">
+    <meta name="application-url" content="http://alf.hubmed.org/2010/07/london-cycle-hire/">
     <link rel="icon" href="icon.png" sizes="60x60">
     <meta name="viewport" content="width=device-width; height=device-height; initial-scale=1.0; maximum-scale=1.0; user-scalable=no">
 
@@ -22,13 +23,14 @@ if (isset($_GET['latitude']) && isset($_GET['longitude'])) {
     <meta name="apple-mobile-web-app-status-bar-style" content="black">
     <meta name="apple-touch-fullscreen" content="yes">
     <link rel="apple-touch-icon" href="icon.png">
-    
+
     <style>
     body { font-family: sans-serif; }
-    th { text-align: left; }
+    th, td { text-align: left; vertical-align: top; padding: 2px 10px 2px 0; }
+    div { margin: 0; padding: 0; }
     </style>
   </head>
-	
+
   <body>
 
     <form method="GET">
@@ -47,21 +49,25 @@ if (isset($_GET['latitude']) && isset($_GET['longitude'])) {
 <? endif; ?>
 <? else: ?>
 <p>Data fetched at <?= date('g:ia \o\n F jS Y', $updated); ?></p>
-  
+
 <table>
   <thead>
     <tr>
+      <th></th>
+      <th>Dist</th>
+      <th>In</th>
+      <th>Out</th>
       <th>Name</th>
-      <th>Available</th>
-      <th>Empty</th>
     </tr>
   </thead>
   <tbody>
 <? foreach ($items as $item): ?>
     <tr>
-      <td><a href="http://maps.google.com/maps?z=18&amp;q=loc:<? h($item['location']['latitude']); ?>+<? h($item['location']['longitude']); ?>"><? h($item['name']); ?></a></td>
+      <td><div style="width:50px; height: 1em; background:red;"><div style="width:<? h(ceil(($item['nbBikes']/($item['nbBikes'] + $item['nbEmptyDocks'])) * 50)); ?>px; height:1em; background:green;"></div></div></td>
+      <td><? h($item['distance']); ?></td>
       <td><? h($item['nbBikes']); ?></td>
       <td><? h($item['nbEmptyDocks']); ?></td>
+      <td><a href="<? h(url('http://maps.google.com/maps', array('z' => 18, 'q' => $item['name'] . '@' . $item['location']['latitude'] . ',' . $item['location']['longitude']))); ?>"><? h($item['name']); ?></a></td>
     </tr>
 <? endforeach; ?>
   </tbody>
@@ -69,20 +75,21 @@ if (isset($_GET['latitude']) && isset($_GET['longitude'])) {
 <? endif; ?>
 
     <script>
-    // geolocation API code from http://owlsnearyou.com/
     if (typeof(navigator.geolocation) != "undefined") {
-  		navigator.geolocation.watchPosition(
-  		  function (position) {
-      	  document.getElementById("latitude").setAttribute("value", position.coords.latitude);
-      	  document.getElementById("longitude").setAttribute("value", position.coords.longitude);
-      	}, 
-      	function (error) {
-      	  var messages = ["", " (permission denied)", " (unavailable)", " (timeout)"];
-      		//alert("Can't get location" + messages[error.code]);
-      	}, 
-      	{ enableHighAccuracy: true,	maximumAge: 600000 }
-  		);
-  	}
-  	</script>
+      navigator.geolocation.watchPosition(
+
+      function (position) {
+        document.getElementById("latitude").setAttribute("value", position.coords.latitude);
+        document.getElementById("longitude").setAttribute("value", position.coords.longitude);
+      }, function (error) {
+        var messages = ["", " (permission denied)", " (unavailable)", " (timeout)"];
+        //alert("Can't get location" + messages[error.code]);
+      }, {
+        enableHighAccuracy: true,
+        maximumAge: 600000
+      });
+    }
+    </script>
   </body>
 </html>
+
--- query.php
+++ query.php
@@ -1,13 +1,23 @@
-<? 
+<?
 
 $mongo = new Mongo('localhost:27017', array('persist' => TRUE));
 $center = array('latitude' => (float) $_GET['latitude'], 'longitude' => (float) $_GET['longitude']);
-$radius = 10.0;
-$items = $mongo->{'london-cycle-hire'}->{'stations'}->find(array('location' => array('$within' => array('$center' => array($center, $radius)))))->limit(25);
-$items = iterator_to_array($items);
+
+//$items = $mongo->{'london-cycle-hire'}->{'stations'}->find(array('location' => array('$near' => $center)))->limit(25);
+//$items = iterator_to_array($items);
+
+$result = $mongo->{'london-cycle-hire'}->command(array('geoNear' => 'stations', 'near' => $center, 'num' => 25));
+$items = $result['results'];
+array_walk($items, 'fix_values');
 
 if ($_GET['_format'] == 'json'){
   header('Content-Type: application/json; charset=UTF-8');
-  print json_encode(array('updated' => $updated, 'stations' => array_values($items)));
+  print json_encode(array('updated' => $updated, 'stations' => $items));
   exit();
 }
+
+function fix_values(&$item){
+  $item['obj']['distance'] = ceil($item['dis'] * 111111);
+  $item = $item['obj'];
+}
+
